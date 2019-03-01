24c4c06bd22bc213959e7bf7457427e5e742ce05
Ogbuji Bright <kelvinyounq@gmail.com>
2019-03-01 23:06:45 +0100

    Arrange system for implementation

--- e-voting/config.php
+++ e-voting/config.php
@@ -7,17 +7,17 @@ define('DB_DATABASE', 'cse');
 
 
 //replace the valid_email with a valid active gmail address
-define('SMTP_USER', 'musa.joshua@lmu.edu.ng');
+define('SMTP_USER', 'ogbuji.bright@lmu.edu.ng');
 
 //replace password with the password of the same account
-define('SMTP_PASSWORD', '41ea45df');
+define('SMTP_PASSWORD', '');
 
 // domain name
 define('HOST', '');
 
 //replace valid_email_address with any email address
 //This is to test if the email sending works at all
-//open the test_mail.php in our browser after setting up
+//open the test_mail.php in your browser after setting up
 // define('TEST_EMAIL', 'valid_email_address')
 
 //NB: Sending email requires internet connectivity
