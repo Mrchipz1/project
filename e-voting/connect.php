837951382982c8fa5d375f1d30fa2d0f3c8eb1b7
Ogbuji Bright <kelvinyounq@gmail.com>
2019-03-22 15:24:15 +0100

    fixing config

--- e-voting/connect.php
+++ e-voting/connect.php
@@ -10,21 +10,21 @@ class dbConnect {
     function connect() {
         // this reaquired the config file and fetch the details that we configured in the config.php file the rest are just initiating the connection 
         require_once('config.php');
-        $server = DB_HOST;
-        $user = DB_USERNAME;
-        $db = DB_DATABASE;
-        $pass = DB_PASSWORD;
-        try {
-            $this->conn = new PDO("mysql:host=$server;dbname=$db", $user, $pass);
-            // set the PDO error mode to exception
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-        catch(PDOException $e)
-            {
-            echo "Connection failed: " . $e->getMessage();
-            }
+        // $server = DB_HOST;
+        // $user = DB_USERNAME;
+        // $db = DB_DATABASE;
+        // $pass = DB_PASSWORD;
+        // try {
+        //     $this->conn = new PDO("mysql:host=$server;dbname=$db", $user, $pass);
+        //     // set the PDO error mode to exception
+        //     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //     }
+        // catch(PDOException $e)
+        //     {
+        //     echo "Connection failed: " . $e->getMessage();
+        //     }
 
-        return $this->conn;
+        // return $this->conn;
     }
 
 }
