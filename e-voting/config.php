daef81937c12c36d772dcda03c8f05dd4552ca70
Ogbuji Bright <kelvinyounq@gmail.com>
2019-03-22 15:29:11 +0100

    fixing config part1

--- e-voting/config.php
+++ e-voting/config.php
@@ -1,5 +1,10 @@
 <?php
 // this is the config file for the project
+// this is the config file for the project
+// define('DB_HOST', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'cse');
 
 /* Database connection settings */
 $host = 'localhost';
@@ -7,5 +12,19 @@ $user = 'root';
 $pass = '';
 $db = 'cse';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+// this is the configuration for smtp so as to use and send mail
+//replace the valid_email with a valid active gmail address
+// define('SMTP_USER', '');
+//replace password with the password of the same account
+// define('SMTP_PASSWORD', '');
+// domain name
+// define('HOST', '');
+//replace valid_email_address with any email address
+//This is to test if the email sending works at all
+//open the test_mail.php in your browser after setting up
+// define('TEST_EMAIL', 'valid_email_address')
+//NB: Sending email requires internet connectivity
+//
 
 ?>
--- e-voting/connect.php
+++ e-voting/connect.php
@@ -1,15 +1,24 @@
-<?php
-//  this is a class for making the connection 
+<?php //  this is a class for making the connection 
 class dbConnect {
     // a vairable where the connection would exist on 
     private $conn;
-    function __construct() {
-    }
-
+    function __construct() {}
     // this function connect would we called in any file that needs a connection to the database
     function connect() {
         // this reaquired the config file and fetch the details that we configured in the config.php file the rest are just initiating the connection 
         require_once('config.php');
+        $server=DB_HOST;
+        $user=DB_USERNAME;
+        $db=DB_DATABASE;
+        $pass=DB_PASSWORD;
+        try {
+            $this->conn=new PDO("mysql:host=$server;dbname=$db", $user, $pass);
+            // set the PDO error mode to exception
+            $this->conn->setAttribute(PDO: : ATTR_ERRMODE, PDO: : ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
         // $server = DB_HOST;
         // $user = DB_USERNAME;
         // $db = DB_DATABASE;
@@ -23,8 +32,6 @@ class dbConnect {
         //     {
         //     echo "Connection failed: " . $e->getMessage();
         //     }
-
-        // return $this->conn;
+        return $this->conn;
     }
-
 }
